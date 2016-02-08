dc949dc171e172a04ab0825e858eca768b8c6fc7
earthworm48 <samkahchiin@gmail.com>
2016-02-08 15:42:35 +0800

    validates the length and presence of password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, length: {maximum: 240},  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
     message: "invalid email address" }, uniqueness: {case_sensitive: false}
     # case_sensitive: to prevent the email of different case (but same content) to pass the validation
+    validates :password, presence: true, length: {minimum: 6}
     has_secure_password
 end
