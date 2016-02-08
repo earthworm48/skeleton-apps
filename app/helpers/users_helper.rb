15c3f5788bc3beb28a5815d2ae6fc92573d76fa5
earthworm48 <samkahchiin@gmail.com>
2016-02-08 16:52:55 +0800

    add gravatar in user#helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+	def gravatar_for(user)
+		# Gravatar URLs are based on an MD5 hash of the user’s email address
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+		# return image tag
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end
+
 end
