cd625adc04ca34739d5643bced02f3ada7040bfb
earthworm48 <samkahchiin@gmail.com>
2016-02-08 15:34:11 +0800

    add has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
+	# in the model, self is optional in the right-hand-side
 	validates :name, presence: true, length: {maximum: 50}
 	validates :email, presence: true, length: {maximum: 240},  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
     message: "invalid email address" }, uniqueness: {case_sensitive: false}
     # case_sensitive: to prevent the email of different case (but same content) to pass the validation
+    has_secure_password
 end
