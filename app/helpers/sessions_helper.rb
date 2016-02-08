c9920c8673037a416f863115c780f01af2f9aaf4
earthworm48 <samkahchiin@gmail.com>
2016-02-08 20:45:48 +0800

    add users fixture

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,16 @@
 module SessionsHelper
+	# Log in the user
 	def log_in(user)
 		session[:user_id] = user.id
 	end
 
+	# Returns the current user if any
 	def current_user
 		@current_user ||= User.find_by(id: session[:user_id])
+	end
+
+	# Returns true if the user is logged in
+	def logged_in?
+		!current_user.nil?
 	end
 end
