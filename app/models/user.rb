5e893c24badeaad8e8d2b5867de58848394ee4bb
earthworm48 <samkahchiin@gmail.com>
2016-02-08 20:50:03 +0800

    add user.digest method

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ActiveRecord::Base
     # case_sensitive: to prevent the email of different case (but same content) to pass the validation
     validates :password, presence: true, length: {minimum: 6}
     has_secure_password
+
+    # Returns the hash digest of the given string so that we can use it in the fixture.yml file
+    def User.digest(string)
+    	cost = ActiveModel::SecurePassword.min_cost? Bcrypt::Engine::MIN_COST : Bcrypt::Engine.cost
+    	Bcrypt::Password.create(string, cost: cost)
+    end
 end
