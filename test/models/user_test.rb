d2608a9b5951cc3360e476f6280d330b09bf23aa
earthworm48 <samkahchiin@gmail.com>
2016-02-08 13:52:21 +0800

    generate should-be-valid test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,5 +2,12 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
+	def setup
+		# generate sample user for testing
+		@user = User.new(name:"Example User",email:"user@example.com")
+	end
 
+	test "should be valid" do
+		assert @user.valid?
+	end
 end
