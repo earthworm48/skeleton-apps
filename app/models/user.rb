a1d693e2e45ecf6bd5f390e6e41b9abd210ee695
earthworm48 <samkahchiin@gmail.com>
2016-02-08 15:18:22 +0800

    validate uniqueness of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
 	validates :name, presence: true, length: {maximum: 50}
-	validates :email, presence: true, length: {maximum: 240}
+	validates :email, presence: true, length: {maximum: 240},  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
+    message: "invalid email address" }, uniqueness: {case_sensitive: false}
+    # case_sensitive: to prevent the email of different case (but same content) to pass the validation
 end
