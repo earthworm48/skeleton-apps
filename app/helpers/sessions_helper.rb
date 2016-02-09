6016d63344095c6ae4353bdf7557a0c8f69d3576
earthworm48 <samkahchiin@gmail.com>
2016-02-09 17:10:26 +0800

    forget method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,12 +13,30 @@ module SessionsHelper
 
 	# Returns the current user if any
 	def current_user
-		@current_user ||= User.find_by(id: session[:user_id])
+		if (user_id = session[:user_id])
+			@current_user ||= User.find_by(id: user_id)
+
+		# if the user_id and remember token is stored in the cookies
+		elsif (user_id = cookies.signed[:user_id])
+			user = User.find_by(id: user_id)
+			if user && user.authenticated?(cookies[:remember_token])
+				log_in user
+				@current_user = user
+			end
+		end
 	end
 
 	# Returns true if the user is logged in
 	def logged_in?
 		!current_user.nil?
+	end
+
+	def remember(user)
+		# separate it into two methods because session controller cannot access some of the 
+		# user method User.new_token in other class
+		user.remember
+		cookies.permanent.signed[:user_id] = user.id
+		cookies.permanent[:remember_token] = user.remember_token
 	end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,32 @@ class User < ActiveRecord::Base
     # case_sensitive: to prevent the email of different case (but same content) to pass the validation
     validates :password, presence: true, length: {minimum: 6}
     has_secure_password
+    # for storage in the cookies without storing it in the database
+    attr_accessor :remember_token
 
     # Returns the hash digest of the given string so that we can use it in the fixture.yml file
-    def User.digest(string)
+    def self.digest(string)
     	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     	# Noted that the 'BCrypt', 'BC' is uppercase
     	BCrypt::Password.create(string, cost: cost)
+    end
+
+    def self.new_token
+    	SecureRandom.urlsafe_base64
+    end
+
+    # Remembers a user in the database for use in persistent sessions.
+    def remember
+    	self.remember_token = User.new_token
+    	update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    # to convert the hash to password string '.new'
+    def authenticated?(remember_token)
+    	BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    def forget
+		update_attribute(:remember_digest, nil)    	
     end
 end
