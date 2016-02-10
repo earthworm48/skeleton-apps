95678c3fb5b0dfc1f296b9392d242c3ba1dcc3cb
earthworm48 <samkahchiin@gmail.com>
2016-02-10 21:41:55 +0800

    add uermailer method to users#create in the controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-      log_in @user
-  		flash[:success] = "Welcome here"
-  		redirect_to @user
+      UserMailer.account_activation(@user)
+      flash[:info] = "Please check your email for activation"
+      redirect_to root_url
   	else
   		render 'new'
   	end
