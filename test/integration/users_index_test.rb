bd8e12cebc0a56fe3e9482a797e1b63db7290324
earthworm48 <samkahchiin@gmail.com>
2016-02-10 19:27:26 +0800

    delete user test (admin)

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -2,20 +2,29 @@ require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
 	def setup
-		@user = users(:michael)
+		@admin = users(:michael)
+		@non_admin = users(:carmen)
 	end
 
-	test "index including pagination" do	
-		log_in_as(@user)
+	test "index as admin including pagination and delete link" do	
+		log_in_as(@admin)
 		get users_path
 		assert_template 'users/index'
 
 		# once using pagination, there will be a div with the class of pagination
 		assert_select 'div.pagination'
-		User.paginate(page: 1).each do |user|
+		# access the first page
+		first_page = User.paginate(page: 1)
+		first_page.each do |user|
 			# there will be a hyperlink <a></a> with the href to the users_path 
-			# and alt which is the name of the user
+			# and display which is the name of the user
 			assert_select 'a[href=?]', user_path(user), text: user.name
+			# check the display -> is it 'delete user' ?
+      assert_select 'a[href=?]', user_path(user), text: 'delete user' if user == @admin
 		end	
+
+		assert_difference 'User.count', -1 do
+			delete user_path(@non_admin)
+		end
 	end
 end
