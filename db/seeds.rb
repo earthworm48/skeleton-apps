93de62ede5ce40b6c52bfdabc2e3e30e9075ebf0
earthworm48 <samkahchiin@gmail.com>
2016-02-10 18:14:56 +0800

    finish seeding users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# The create! method is just like the create method, except it raises an exception (Section 6.1.4) for an invalid user rather than returning false.
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+99.times do |n|
+	name = Faker::Name.name
+	email = "example-#{n+1}@railstutorial.org"
+	password = "password"
+	User.create!(name: name, email: email, password: password, password_confirmation: password)
+end
