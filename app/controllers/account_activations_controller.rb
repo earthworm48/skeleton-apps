8b11e7773b31d59f6b488dffd98607597e211a80
earthworm48 <samkahchiin@gmail.com>
2016-02-10 21:40:25 +0800

    mailer test

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,8 @@
 class AccountActivationsController < ApplicationController
+
+	def edit
+		@user = User.find_by(email: params[:email])
+		render 'users/edit'
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'sign_up' => 'users#new'
 
   resources :users
+  resources :account_activations, only: [:edit]
   root 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
