9219e230f575f245bf89c37a3916b3007ba20d2e
earthworm48 <samkahchiin@gmail.com>
2016-02-11 17:58:32 +0800

    smtp setting for gmail mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,9 +15,25 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'localhost:3000'
-  config.action_mailer.default_url_options = { host: host }
+  
+  # for preview email only
+  # config.action_mailer.delivery_method = :test
+  # host = 'localhost:3000'
+  # config.action_mailer.default_url_options = { host: host }
+
+  # for sending using gmail
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => ENV['gmail'],
+   :password             => ENV['gmail_password'],
+   :authentication       => "plain",
+  :enable_starttls_auto => true
+}
+
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
